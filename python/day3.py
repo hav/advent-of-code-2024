276ce706a4b1e7cf3889aead16f0c27c9645effa
Farshid Besharati <farshid.besharati@volvocars.com>
2024-12-18 15:36:57 +0100

    day3 part 2

--- python/day3.py
+++ python/day3.py
@@ -5,39 +5,54 @@ import unittest
 
 dataFile = "day3_data.txt"
 
-testData = """xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"""
+p1TestData = (
+    """xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"""
+)
 
-
-def parseInput(inputData, regex):
-    return re.findall(regex, inputData)
+p2TestData = (
+    """xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"""
+)
 
 
 def part1(inputData):
     regex = r"mul\((\d+),(\d+)\)"
-    ops = parseInput(inputData, regex)
-    return sum(int(i) * int(j) for i, j in ops)
+    return sum(int(i) * int(j) for i, j in re.findall(regex, inputData))
 
 
 def part2(inputData):
-    pass
+    regex = r"don't|do|mul\((\d+),(\d+)\)"
+    ops = re.finditer(regex, inputData)
+
+    do = 1
+    mul = 0
+
+    for i in ops:
+        if i.groups()[0] and do:
+            mul += int(i.groups()[0]) * int(i.groups()[1])
+        elif (i.group(0)) == "do":
+            do = 1
+        elif (i.group(0)) == "don't":
+            do = 0
+
+    return mul
 
 
 class Day2Test(unittest.TestCase):
     def test_Part1WithTestData(self):
-        self.assertEqual(part1(testData), 161)
+        self.assertEqual(part1(p1TestData), 161)
 
     def test_Part2WithTestData(self):
-        self.assertEqual(part2(testData), 4)
+        self.assertEqual(part2(p2TestData), 48)
 
     def test_Part1(self):
         with open(Path(__file__).parent.joinpath(dataFile), "r") as inputFile:
             inputData = inputFile.read()
             self.assertEqual(part1(inputData), 190604937)
 
-    # def test_Part2(self):
-    #     with open(Path(__file__).parent.joinpath(dataFile), "r") as inputFile:
-    #         inputData = inputFile.readlines()
-    #         self.assertEqual(part2(inputData), 569)
+    def test_Part2(self):
+        with open(Path(__file__).parent.joinpath(dataFile), "r") as inputFile:
+            inputData = inputFile.read()
+            self.assertEqual(part2(inputData), 82857512)
 
 
 if __name__ == "__main__":
